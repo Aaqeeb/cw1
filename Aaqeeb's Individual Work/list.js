73215fcb4a8ee0a4fb39cce48eae73486fd8428f
Aaqeeb <100525623+Aaqeeb@users.noreply.github.com>
2022-04-13 14:14:08 +0400

    Add files via upload

---
+++ Aaqeeb's Individual Work/list.js
@@ -0,0 +1,48 @@
+var list = [
+    {
+      "id": 1001,
+      "title": "English",
+      "description": "Learn English .",
+      "price": 100.00,
+      "image": "https://images.unsplash.com/photo-1599229526921-4f29d42b0b41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGllcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+      "availableInventory": 10,
+      "rating": 2
+    },
+    {
+      "id": 1002,
+      "title": "French",
+      "description": "Explore the beauty of the French Language.",
+      "price": 30.49,
+      "image": "https://media.istockphoto.com/photos/string-light-bulbs-at-sunset-picture-id1300384615?b=1&k=20&m=1300384615&s=170667a&w=0&h=rkDm5TdJp_dU7VAknk4EuZEZ2ho2QQspOavjlwGrsuI=",
+      "availableInventory": 17,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Biology",
+      "description": "Know more about your body.",
+      "price": 150.45,
+      "image": "https://images.unsplash.com/photo-1616715076865-1f6f05988ff2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dW5zcGxhc2glMjBhcHB8ZW58MHx8MHx8&w=1000&q=80",
+      "availableInventory": 200,
+      "rating": 5
+    },
+    {
+      "id": 1004,
+      "title": "Arabic",
+      "description": "Learn Arabic in two weeks.",
+      "price": 50.99,
+      "image": "https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60/cat-house.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    {
+      "id": 1005,
+      "title": "Mathematics",
+      "description": "Become a Math wizard.",
+      "price": 49.99,
+      "image": "https://media.istockphoto.com/photos/ramadan-kareem-lantern-and-dates-fruit-with-city-light-background-picture-id1305026622?b=1&k=20&m=1305026622&s=170667a&w=0&h=EEqBsXLytjJGv5oQaxnGB19SnFhgj-c7z30FLgmwZiQ=",
+      "availableInventory": 25,
+      "rating": 4
+    }
+  ]
+  
